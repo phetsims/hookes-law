bf8b84acefd4dd82c89638b6bd3778b35ef808a3
Chris Malley <cmalley@pixelzoom.com>
2015-04-03 15:00:38 -0600

    Return buttonπ

--- js/introduction/view/IntroductionView.js
+++ js/introduction/view/IntroductionView.js
@@ -12,9 +12,13 @@ define( function( require ) {
   var HookesLawConstants = require( 'HOOKES_LAW/common/HookesLawConstants' );
   var HookesLawFont = require( 'HOOKES_LAW/common/HookesLawFont' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var Text = require( 'SCENERY/nodes/Text' );
+
+  // strings
+  var returnString = require( 'string!HOOKES_LAW/return' );
 
   /**
    * @param {IntroductionModel} model
@@ -29,15 +33,27 @@ define( function( require ) {
       center: this.layoutBounds.center
     } ) );
 
-    // Create and add the Reset All Button in the bottom right, which resets the model
+    // Reset All button, bottom right
     var resetAllButton = new ResetAllButton( {
       listener: function() {
         model.reset();
       },
-      right:  this.layoutBounds.maxX - 10,
+      right: this.layoutBounds.maxX - 10,
       bottom: this.layoutBounds.maxY - 10
     } );
     this.addChild( resetAllButton );
+
+    // Return button
+    var returnButton = new RectangularPushButton( {
+      content: new Text( returnString, { font: new HookesLawFont( 24 ) } ),
+      baseColor: 'rgb( 230, 230, 230 )',
+      right: resetAllButton.left - 30,
+      centerY: resetAllButton.centerY,
+      listener: function() {
+        //TODO
+      }
+    } );
+    this.addChild( returnButton );
   }
 
   return inherit( ScreenView, IntroductionView, {
