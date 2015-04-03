401549bae9c410f3b905128f73f5ddca6faf0e2e
Chris Malley <cmalley@pixelzoom.com>
2015-04-03 14:30:40 -0600

    colors

---
+++ js/common/HookesLawColors.js
@@ -0,0 +1,19 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ * Colors for this simulation.
+ *
+ * @author Chris Malley
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
+
+  return {
+    APPLIED_FORCE_VECTOR: PhetColorScheme.RED_COLORBLIND,
+    SPRING_FORCE_VECTOR: 'blue',
+    DISPLACEMENT_VECTOR: 'green'
+  };
+} );
