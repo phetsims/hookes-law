fad6faba335fca48973bb7296d0e6ec47431be42
Chris Malley <cmalley@pixelzoom.com>
2015-04-20 12:49:23 -0600

    Left align vector icons in control panel

--- js/common/view/VisibilityPanel.js
+++ js/common/view/VisibilityPanel.js
@@ -14,6 +14,7 @@ define( function( require ) {
   var HBox = require( 'SCENERY/nodes/HBox' );
   var HookesLawColors = require( 'HOOKES_LAW/common/HookesLawColors' );
   var HookesLawFont = require( 'HOOKES_LAW/common/HookesLawFont' );
+  var HStrut = require( 'SUN/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Panel = require( 'SUN/Panel' );
   var Property = require( 'AXON/Property' );
@@ -44,9 +45,29 @@ define( function( require ) {
       yMargin: 15
     }, options );
 
-    var appliedForceCheckBox = createVectorCheckBox( visibilityProperties.appliedForceVectorVisibleProperty, appliedForceString, HookesLawColors.APPLIED_FORCE_VECTOR );
-    var springForceCheckBox = createVectorCheckBox( visibilityProperties.springForceVectorVisibleProperty, springForceString, HookesLawColors.SPRING_FORCE_VECTOR );
-    var displacementCheckBox = createVectorCheckBox( visibilityProperties.displacementVectorVisibleProperty, displacementString, HookesLawColors.DISPLACEMENT_VECTOR );
+    // text labels on the vector check boxes
+    var appliedForceTextNode = new Text( appliedForceString, TEXT_OPTIONS );
+    var springForceTextNode = new Text( springForceString, TEXT_OPTIONS );
+    var displacementTextNode = new Text( displacementString, TEXT_OPTIONS );
+    var maxTextWidth = _.max( [ appliedForceTextNode, springForceTextNode, displacementTextNode ], function( node ) {
+      return node.width;
+    } ).width;
+
+    // vector check boxes, with left-aligned vector icons
+    var minTextArrowSpacing = 10;
+    var appliedForceCheckBox = createVectorCheckBox( visibilityProperties.appliedForceVectorVisibleProperty, appliedForceTextNode, {
+      vectorColor: HookesLawColors.APPLIED_FORCE_VECTOR,
+      textVectorSpacing: maxTextWidth - appliedForceTextNode.width + minTextArrowSpacing
+    } );
+    var springForceCheckBox = createVectorCheckBox( visibilityProperties.springForceVectorVisibleProperty, springForceTextNode, {
+      vectorColor: HookesLawColors.SPRING_FORCE_VECTOR,
+      textVectorSpacing: maxTextWidth - springForceTextNode.width + minTextArrowSpacing
+    } );
+    var displacementCheckBox = createVectorCheckBox( visibilityProperties.displacementVectorVisibleProperty, displacementTextNode, {
+      vectorColor: HookesLawColors.DISPLACEMENT_VECTOR,
+      textVectorSpacing: maxTextWidth - displacementTextNode.width + minTextArrowSpacing
+    } );
+
     var equilibriumPositionCheckBox = new CheckBox( new Text( equilibriumPositionString, TEXT_OPTIONS ), visibilityProperties.equilibriumPositionVisibleProperty );
     var valuesCheckBox = new CheckBox( new Text( valuesString, TEXT_OPTIONS ), visibilityProperties.valuesVisibleProperty, CHECK_BOX_OPTIONS );
 
@@ -76,13 +97,13 @@ define( function( require ) {
    * Creates a check box for controlling visibility of a vector
    *
    * @param {Property.<boolean>} property
-   * @param {string} text text, positioned to the left of the vector
-   * @param {string|Color} vectorColor vector color
+   * @param {Node} textNode text, positioned to the left of the vector
    * @param {Object} [options]
    */
-  var createVectorCheckBox = function( property, text, vectorColor, options ) {
+  var createVectorCheckBox = function( property, textNode, options ) {
 
     options = _.extend( {
+      vectorColor: 'white', // {string|Color}
       textVectorSpacing: 10, // {number} horizontal space between the text and vector
       vectorDirection: 'right' // {string} direction that the vector points, 'left' or 'right'
     }, options );
@@ -90,16 +111,16 @@ define( function( require ) {
     // text and vector
     var icon = new HBox( {
       children: [
-        new Text( text, TEXT_OPTIONS ),
+        textNode,
+        new HStrut( options.textVectorSpacing ),
         new ArrowNode( 0, 0, ( options.vectorDirection === 'left' ? -30 : 30 ), 0, {
-          fill: vectorColor,
+          fill: options.vectorColor,
           headWidth: 20,
           headHeight: 10,
           tailWidth: 10
         } )
       ],
-      align: 'center',
-      spacing: options.textVectorSpacing
+      align: 'center'
     } );
 
     return new CheckBox( icon, property, CHECK_BOX_OPTIONS );
