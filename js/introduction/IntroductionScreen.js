4c43a534837b15ce40355a17f61b63f50c61506f
Chris Malley <cmalley@pixelzoom.com>
2015-04-21 14:42:09 -0600

    adjust mvt for new displacement range

--- js/introduction/IntroductionScreen.js
+++ js/introduction/IntroductionScreen.js
@@ -31,7 +31,7 @@ define( function( require ) {
   function IntroductionScreen() {
 
     // Scale up for the view
-    var modelViewTransform = ModelViewTransform2.createOffsetScaleMapping( Vector2.ZERO, 15 );
+    var modelViewTransform = ModelViewTransform2.createOffsetScaleMapping( Vector2.ZERO, 200 );
 
     Screen.call( this,
       introductionString,
