4daa32c3446048ea7b58e1d57bd197f9c2c6ebfa
Chris Malley <cmalley@pixelzoom.com>
2015-05-07 14:28:21 -0600

    first pass at RoboticArm model, not wired up

---
+++ js/common/model/RoboticArm.js
@@ -0,0 +1,35 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ * The robotic arm.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  /**
+   * @param {Object} [object]
+   * @constructor
+   */
+  function RoboticArm( options ) {
+
+    options = _.extend( {
+      x: 0, // {number} initial x location of the fixed end of the arm, units = m
+      hookXOffset: 1  // {number} hookXOffset initial x-offset of the hook (movable end of the arm), units = m
+    }, options );
+
+    this.x = options.x; // read-only
+
+    PropertySet.call( this, {
+      hookXOffset: options.hookXOffset
+    } );
+  }
+
+  return inherit( PropertySet, RoboticArm );
+} );
