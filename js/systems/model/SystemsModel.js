7de9024e10cb133a2d68e923a2db22fcb60046ce
Chris Malley <cmalley@pixelzoom.com>
2015-05-27 14:25:50 -0600

    Keep spring constants and applied force synchronized between serial and parallel systems.

--- js/systems/model/SystemsModel.js
+++ js/systems/model/SystemsModel.js
@@ -17,8 +17,38 @@ define( function( require ) {
    * @constructor
    */
   function SystemsModel() {
+
+    var thisModel = this;
+
     this.seriesSystem = new SeriesSystem();
     this.parallelSystem = new ParallelSystem();
+
+    // synchronize applied force F between the 2 systems
+    assert && assert( this.seriesSystem.equivalentSpring.appliedForceRange.equals( thisModel.parallelSystem.equivalentSpring.appliedForceRange ) );
+    this.seriesSystem.equivalentSpring.appliedForceProperty.link( function( appliedForce ) {
+      thisModel.parallelSystem.equivalentSpring.appliedForceProperty.set( appliedForce );
+    } );
+    this.parallelSystem.equivalentSpring.appliedForceProperty.link( function( appliedForce ) {
+      thisModel.seriesSystem.equivalentSpring.appliedForceProperty.set( appliedForce );
+    } );
+
+    // synchronize spring constant k1 (left and top) between the 2 systems
+    assert && assert( this.seriesSystem.leftSpring.springConstantRange.equals( thisModel.parallelSystem.topSpring.springConstantRange ) );
+    this.seriesSystem.leftSpring.springConstantProperty.link( function( springConstant ) {
+      thisModel.parallelSystem.topSpring.springConstantProperty.set( springConstant );
+    } );
+    this.parallelSystem.topSpring.springConstantProperty.link( function( springConstant ) {
+      thisModel.seriesSystem.leftSpring.springConstantProperty.set( springConstant );
+    } );
+
+    // synchronize spring constant k2 (right and bottom) between the 2 systems
+    assert && assert( this.seriesSystem.rightSpring.springConstantRange.equals( thisModel.parallelSystem.bottomSpring.springConstantRange ) );
+    this.seriesSystem.rightSpring.springConstantProperty.link( function( springConstant ) {
+      thisModel.parallelSystem.bottomSpring.springConstantProperty.set( springConstant );
+    } );
+    this.parallelSystem.bottomSpring.springConstantProperty.link( function( springConstant ) {
+      thisModel.seriesSystem.rightSpring.springConstantProperty.set( springConstant );
+    } );
   }
 
   return inherit( Object, SystemsModel, {
