6a19c85b9e6c1fd583785c4a9b2be27b76f22c15
Chris Malley <cmalley@pixelzoom.com>
2015-06-01 17:38:17 -0600

    view-specific properties for Energy screen

--- js/energy/view/EnergyView.js
+++ js/energy/view/EnergyView.js
@@ -9,6 +9,7 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var EnergyViewProperties = require( 'HOOKES_LAW/energy/view/EnergyViewProperties' );
   var HookesLawConstants = require( 'HOOKES_LAW/common/HookesLawConstants' );
   var inherit = require( 'PHET_CORE/inherit' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
@@ -20,7 +21,11 @@ define( function( require ) {
    */
   function EnergyView( model ) {
 
+    var thisView = this;
     ScreenView.call( this, HookesLawConstants.SCREEN_VIEW_OPTIONS );
+
+    // Properties that are specific to the view
+    var viewProperties = new EnergyViewProperties();
 
     // Create and add the Reset All Button in the bottom right, which resets the model
     var resetAllButton = new ResetAllButton( {
---
+++ js/energy/view/EnergyViewProperties.js
@@ -0,0 +1,33 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ * Properties that are specific to visibility of things in the "Energy" view.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var HookesLawQueryParameters = require( 'HOOKES_LAW/common/HookesLawQueryParameters' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require( 'AXON/PropertySet' );
+
+  /**
+   * @constructor
+   */
+  function IntroductionViewProperties() {
+
+    // to make development easier
+    var checked = HookesLawQueryParameters.DEV ? true : false;
+
+    PropertySet.call( this, {
+      graph: 'force', // {string} which graph is visible, 'force'|'energy'
+      displacementVectorVisible: checked, // {boolean} is the displacement vector visible?
+      equilibriumPositionVisible: checked, // {boolean} is the equilibrium position visible?
+      valuesVisible: checked  // {boolean} are numeric values visible?
+    } );
+  }
+
+  return inherit( PropertySet, IntroductionViewProperties );
+} );
