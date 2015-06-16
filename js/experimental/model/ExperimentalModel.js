52d8b92acfccde58f2d92d5a75dc132fd7164293
Chris Malley <cmalley@pixelzoom.com>
2015-06-16 14:23:04 -0600

    set default values to something that resembles spring "look" in design doc

--- js/experimental/model/ExperimentalModel.js
+++ js/experimental/model/ExperimentalModel.js
@@ -19,13 +19,13 @@ define( function( require ) {
   function ExperimentalModel() {
 
     // ranges and default values for properties
-    this.radiusRange = new Range( 20, 70, 50 );
-    this.aspectRatioRange = new Range( 0.5, 3, 1 );
-    this.pointsPerLoopRange = new Range( 10, 100, 50 );
+    this.radiusRange = new Range( 20, 70, 30 );
+    this.aspectRatioRange = new Range( 0.5, 3, 1.5 );
+    this.pointsPerLoopRange = new Range( 10, 100, 30 );
     this.lineWidthRange = new Range( 1, 10, 3 );
     this.phaseRange = new Range( 0, 10, 0 );
-    this.deltaPhaseRange = new Range( 0, 7, 1 );
-    this.pitchSizeRange = new Range( 0.1, 2, 1 );
+    this.deltaPhaseRange = new Range( 0, 7, 1.5 );
+    this.pitchSizeRange = new Range( 0.1, 2, 1.5 );
 
     PropertySet.call( this, {
       radius: this.radiusRange.defaultValue, // {number} radius of a loop with aspect ratio of 1:1
