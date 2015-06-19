81074974a5d572063841eab68470e902c3ae7202
Chris Malley <cmalley@pixelzoom.com>
2015-06-19 16:18:37 -0600

    doc

--- js/common/HookesLawColors.js
+++ js/common/HookesLawColors.js
@@ -13,9 +13,13 @@ define( function( require ) {
   var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
 
   return {
+
+    // UI components
     CONTROL_PANEL_FILL: new Color( 243, 243, 243 ),
     CONTROL_PANEL_STROKE: new Color( 125, 125, 125 ),
     SEPARATOR_STROKE: new Color( 125, 125, 125 ),
+
+    // various quantities
     APPLIED_FORCE: PhetColorScheme.RED_COLORBLIND,
     TOTAL_SPRING_FORCE: new Color( 11, 37, 251 ),
     LEFT_SPRING_FORCE: new Color( '#6D00FF' ),
