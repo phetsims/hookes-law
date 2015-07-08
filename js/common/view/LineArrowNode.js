7aaeffee9bb3b1794a5667f2b3df24ab7f802c78
Chris Malley <cmalley@pixelzoom.com>
2015-07-08 11:35:08 -0600

    update TODO

--- js/common/view/LineArrowNode.js
+++ js/common/view/LineArrowNode.js
@@ -1,6 +1,6 @@
 // Copyright 2002-2015, University of Colorado Boulder
 
-//TODO scenery-phet#165 reimplement as a Path and move to common code
+//TODO scenery-phet#165 reimplement as LineArrowShape and a Path subtype, move to scenery-phet
 /**
  * An arrow that is composed of 3 line segments: one for the tail, and 2 for a V-shaped head
  *
