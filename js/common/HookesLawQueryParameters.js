b263f2c1c85df46cef0e36a65e2bb42e5a2354ac
Chris Malley <cmalley@pixelzoom.com>
2018-08-21 10:38:35 -0600

    doc
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/HookesLawQueryParameters.js
+++ js/common/HookesLawQueryParameters.js
@@ -16,8 +16,8 @@ define( function( require ) {
     // checks all Check Boxes, to make development easier
     checkAll: { type: 'flag' },
 
-    // Determines whether update cycles are allowed for Properties instrumented for PhET-iO.
-    // Set this to false to identify such cycles. It will likely be impossible to eliminate all cycles.
+    // Determines whether update cycles are allowed for some key Properties.
+    // Set this to false to identify such cycles. (It was not possible to eliminate all cycles.)
     // See https://github.com/phetsims/hookes-law/issues/52
     reentrant: {
       type: 'boolean',
