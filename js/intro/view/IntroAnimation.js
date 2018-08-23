e9fc3b435a47aed68242910f774d11984e788626
Chris Malley <cmalley@pixelzoom.com>
2018-08-23 17:17:43 -0600

    make animation Properties persistent, #62
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/intro/view/IntroAnimation.js
+++ js/intro/view/IntroAnimation.js
@@ -4,7 +4,7 @@
  * The animation that occurs when you switch between 1 and 2 systems in the Intro screen.
  * This is a chain of animations that is applied sequentially to the 2 system Nodes.
  *
- * @author Chris Malley (PixelZoom, Inc.)
+ * @author Chris Malley (PixelZoom, Inc.)                                                                                                                                     x
  */
 define( function( require ) {
   'use strict';
@@ -15,7 +15,6 @@ define( function( require ) {
   var Emitter = require( 'AXON/Emitter' );
   var hookesLaw = require( 'HOOKES_LAW/hookesLaw' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var NumberProperty = require( 'AXON/NumberProperty' );
 
   // constants
   var STEPPER = 'manual'; // step must be called by the client
@@ -23,14 +22,16 @@ define( function( require ) {
   var OPACITY_DURATION = 0.5; // duration of system 2 opacity animation, in seconds
 
   /**
-   * @param {Node} system1Node
-   * @param {Node} system2Node
    * @param {number} numberOfSystems
    * @param {Bounds2} layoutBounds - of the associated ScreenView
+   * @param {NumberProperty} system1CenterYProperty - centerY of the Node for system 1
+   * @param {NumberProperty} system2OpacityProperty - opacity of the Node for system 2
+   * @param {Node} system2Node
    * @param {Tandem} tandem
    * @constructor
    */
-  function IntroAnimation( system1Node, system2Node, numberOfSystems, layoutBounds, tandem ) {
+  function IntroAnimation( numberOfSystems, layoutBounds,
+                           system1CenterYProperty, system2OpacityProperty, system2Node, tandem ) {
 
     assert && assert( numberOfSystems === 1 || numberOfSystems === 2, 'invalid numberOfSystems: ' + numberOfSystems );
 
@@ -41,23 +42,6 @@ define( function( require ) {
 
     // @public emit() is called when the animation completes
     this.finishEmitter = new Emitter();
-
-    // Vertical position of system 1, instrumented for PhET-iO to support record/playback, see #53.
-    var system1CenterYProperty = new NumberProperty( system1Node.centerY, {
-      tandem: tandem.createTandem( 'system1CenterYProperty' )
-    } );
-    system1CenterYProperty.link( function( centerY ) {
-      system1Node.centerY = centerY;
-    } );
-
-    // Opacity of system 2, instrumented for PhET-iO to support record/playback, see #53.
-    var system2OpacityProperty = new NumberProperty( system2Node.opacity, {
-      isValidValue: function( value ) { return value >= 0 && value <= 1; },
-      tandem: tandem.createTandem( 'system2OpacityProperty' )
-    } );
-    system2OpacityProperty.link( function( opacity ) {
-      system2Node.opacity = opacity;
-    } );
 
     var translateSystem1 = null; // {Animation} for system 1 translation
     var fadeSystem2 = null; // {Animation} for system 2 fade (opacity)
--- js/intro/view/IntroScreenView.js
+++ js/intro/view/IntroScreenView.js
@@ -17,6 +17,7 @@ define( function( require ) {
   var IntroSystemNode = require( 'HOOKES_LAW/intro/view/IntroSystemNode' );
   var IntroViewProperties = require( 'HOOKES_LAW/intro/view/IntroViewProperties' );
   var IntroVisibilityControls = require( 'HOOKES_LAW/intro/view/IntroVisibilityControls' );
+  var NumberProperty = require( 'AXON/NumberProperty' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var VBox = require( 'SCENERY/nodes/VBox' );
@@ -96,11 +97,31 @@ define( function( require ) {
     } );
     this.addChild( resetAllButton );
 
+    // Vertical position of system 1.
+    // Instrumented and persistent for PhET-iO to support record/playback, see #53 and #62.
+    var system1CenterYProperty = new NumberProperty( system1Node.centerY, {
+      tandem: tandem.createTandem( 'system1CenterYProperty' )
+    } );
+    system1CenterYProperty.link( function( centerY ) {
+      system1Node.centerY = centerY;
+    } );
+
+    // Opacity of system 2.
+    // Instrumented and persistent for PhET-iO to support record/playback, see #53 and #62.
+    var system2OpacityProperty = new NumberProperty( system2Node.opacity, {
+      isValidValue: function( value ) { return value >= 0 && value <= 1; },
+      tandem: tandem.createTandem( 'system2OpacityProperty' )
+    } );
+    system2OpacityProperty.link( function( opacity ) {
+      system2Node.opacity = opacity;
+    } );
+
     // @private When the number of systems changes, animate the systems.
     this.animation = null;
     viewProperties.numberOfSystemsProperty.lazyLink( function( numberOfSystems ) {
       self.animation && self.animation.stop();
-      self.animation = new IntroAnimation( system1Node, system2Node, numberOfSystems, self.layoutBounds, tandem );
+      self.animation = new IntroAnimation( numberOfSystems, self.layoutBounds,
+        system1CenterYProperty, system2OpacityProperty, system2Node, tandem );
       self.animation.finishEmitter.addListener( function() { self.animation = null; } );
       self.animation.start();
     } );
