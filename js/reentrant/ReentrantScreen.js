d8b237ffc90e870356778ca5e2b8f51448aae89e
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:44 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/reentrant/ReentrantScreen.js
+++ js/reentrant/ReentrantScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * Reentrant screen demonstrates the relationship between reentrant Properties in hookes-law,
