cd5bc19fcbe3333f3472c54c336d9ffe72c7bddd
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:27 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/hookes-law-phet-io-elements-baseline.js
+++ js/phet-io/hookes-law-phet-io-elements-baseline.js
@@ -5371,7 +5371,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "hookesLaw.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
