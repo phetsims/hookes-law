0e40714d0d996a380ab71e5ca0fb1b585fabaf0f
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:07 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/hookesLawStrings.ts
@@ -0,0 +1,52 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import hookesLaw from './hookesLaw.js';
+
+type StringsType = {
+  'appliedForce': string,
+  'appliedForceColon': string,
+  'appliedForceNumber': string,
+  'barGraph': string,
+  'bottomSpring': string,
+  'components': string,
+  'displacement': string,
+  'displacementColon': string,
+  'energy': string,
+  'energyPlot': string,
+  'equilibriumPosition': string,
+  'forcePlot': string,
+  'hookes-law': {
+    'title': string
+  },
+  'intro': string,
+  'joules': string,
+  'leftSpring': string,
+  'meters': string,
+  'newtons': string,
+  'newtonsPerMeter': string,
+  'pattern': {
+    '0value': {
+      '1units': string
+    }
+  },
+  'potentialEnergy': string,
+  'rightSpring': string,
+  'springConstant': string,
+  'springConstantNumber': string,
+  'springForce': string,
+  'systems': string,
+  'topSpring': string,
+  'total': string,
+  'values': string
+};
+
+const hookesLawStrings = getStringModule( 'HOOKES_LAW' ) as StringsType;
+
+hookesLaw.register( 'hookesLawStrings', hookesLawStrings );
+
+export default hookesLawStrings;
