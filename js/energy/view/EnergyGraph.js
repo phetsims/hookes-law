d7740caefe754e1b2162d82954acaec87810d4df
phet-dev <phetadmin@gmail.com>
2022-01-07 03:23:46 -0700

    update copyright dates from daily grunt work

--- js/energy/view/EnergyGraph.js
+++ js/energy/view/EnergyGraph.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * EnergyGraph enumerates the graph choices in the Energy screen.
--- js/systems/view/SystemType.js
+++ js/systems/view/SystemType.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 import EnumerationDeprecated from '../../../../phet-core/js/EnumerationDeprecated.js';
 import hookesLaw from '../../hookesLaw.js';
