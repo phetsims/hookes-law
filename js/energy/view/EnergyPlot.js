3441ac94a1ec703cd1cd5914ce989b1e3301986b
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:08:41 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/energy/view/EnergyPlot.js
+++ js/energy/view/EnergyPlot.js
@@ -6,7 +6,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import { Path } from '../../../../scenery/js/imports.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
