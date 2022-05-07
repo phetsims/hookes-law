69266c6a11ed4e0ae8928fe34e9c0601ed5a63f1
Sam Reid <reids@colorado.edu>
2022-05-07 09:36:12 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/hookesLawStrings.ts
+++ js/hookesLawStrings.ts
@@ -8,41 +8,41 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import hookesLaw from './hookesLaw.js';
 
 type StringsType = {
-  'appliedForce': string,
-  'appliedForceColon': string,
-  'appliedForceNumber': string,
-  'barGraph': string,
-  'bottomSpring': string,
-  'components': string,
-  'displacement': string,
-  'displacementColon': string,
-  'energy': string,
-  'energyPlot': string,
-  'equilibriumPosition': string,
-  'forcePlot': string,
+  'appliedForce': string;
+  'appliedForceColon': string;
+  'appliedForceNumber': string;
+  'barGraph': string;
+  'bottomSpring': string;
+  'components': string;
+  'displacement': string;
+  'displacementColon': string;
+  'energy': string;
+  'energyPlot': string;
+  'equilibriumPosition': string;
+  'forcePlot': string;
   'hookes-law': {
-    'title': string
-  },
-  'intro': string,
-  'joules': string,
-  'leftSpring': string,
-  'meters': string,
-  'newtons': string,
-  'newtonsPerMeter': string,
+    'title': string;
+  };
+  'intro': string;
+  'joules': string;
+  'leftSpring': string;
+  'meters': string;
+  'newtons': string;
+  'newtonsPerMeter': string;
   'pattern': {
     '0value': {
-      '1units': string
+      '1units': string;
     }
-  },
-  'potentialEnergy': string,
-  'rightSpring': string,
-  'springConstant': string,
-  'springConstantNumber': string,
-  'springForce': string,
-  'systems': string,
-  'topSpring': string,
-  'total': string,
-  'values': string
+  };
+  'potentialEnergy': string;
+  'rightSpring': string;
+  'springConstant': string;
+  'springConstantNumber': string;
+  'springForce': string;
+  'systems': string;
+  'topSpring': string;
+  'total': string;
+  'values': string;
 };
 
 const hookesLawStrings = getStringModule( 'HOOKES_LAW' ) as StringsType;
