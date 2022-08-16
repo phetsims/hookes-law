e1999af958cbc7d879fd6254c31a94c9b9b14121
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/hookesLawStrings.ts
+++ js/hookesLawStrings.ts
@@ -5,44 +5,74 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import hookesLaw from './hookesLaw.js';
 
 type StringsType = {
   'appliedForce': string;
+  'appliedForceProperty': TReadOnlyProperty<string>;
   'appliedForceColon': string;
+  'appliedForceColonProperty': TReadOnlyProperty<string>;
   'appliedForceNumber': string;
+  'appliedForceNumberProperty': TReadOnlyProperty<string>;
   'barGraph': string;
+  'barGraphProperty': TReadOnlyProperty<string>;
   'bottomSpring': string;
+  'bottomSpringProperty': TReadOnlyProperty<string>;
   'components': string;
+  'componentsProperty': TReadOnlyProperty<string>;
   'displacement': string;
+  'displacementProperty': TReadOnlyProperty<string>;
   'displacementColon': string;
+  'displacementColonProperty': TReadOnlyProperty<string>;
   'energy': string;
+  'energyProperty': TReadOnlyProperty<string>;
   'energyPlot': string;
+  'energyPlotProperty': TReadOnlyProperty<string>;
   'equilibriumPosition': string;
+  'equilibriumPositionProperty': TReadOnlyProperty<string>;
   'forcePlot': string;
+  'forcePlotProperty': TReadOnlyProperty<string>;
   'hookes-law': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'intro': string;
+  'introProperty': TReadOnlyProperty<string>;
   'joules': string;
+  'joulesProperty': TReadOnlyProperty<string>;
   'leftSpring': string;
+  'leftSpringProperty': TReadOnlyProperty<string>;
   'meters': string;
+  'metersProperty': TReadOnlyProperty<string>;
   'newtons': string;
+  'newtonsProperty': TReadOnlyProperty<string>;
   'newtonsPerMeter': string;
+  'newtonsPerMeterProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0value': {
       '1units': string;
+      '1unitsProperty': TReadOnlyProperty<string>;
     }
   };
   'potentialEnergy': string;
+  'potentialEnergyProperty': TReadOnlyProperty<string>;
   'rightSpring': string;
+  'rightSpringProperty': TReadOnlyProperty<string>;
   'springConstant': string;
+  'springConstantProperty': TReadOnlyProperty<string>;
   'springConstantNumber': string;
+  'springConstantNumberProperty': TReadOnlyProperty<string>;
   'springForce': string;
+  'springForceProperty': TReadOnlyProperty<string>;
   'systems': string;
+  'systemsProperty': TReadOnlyProperty<string>;
   'topSpring': string;
+  'topSpringProperty': TReadOnlyProperty<string>;
   'total': string;
+  'totalProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
 };
 
 const hookesLawStrings = getStringModule( 'HOOKES_LAW' ) as StringsType;
