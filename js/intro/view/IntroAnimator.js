662816a7e35c7d2088107dbf6c0233e6394af515
pixelzoom <cmalley@pixelzoom.com>
2022-12-26 16:49:35 -0700

    // @ts-nocheck

--- js/intro/view/IntroAnimator.js
+++ js/intro/view/IntroAnimator.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * IntroAnimator is responsible for animating the transition between 1 and 2 systems in the Intro screen.
  *
