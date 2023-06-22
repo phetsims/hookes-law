e95c92e2b83ba409fbc145952b696543a6479063
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-22 17:32:27 -0600

    convert to Disposable.assertNotDisposable(), https://github.com/phetsims/axon/issues/436

--- js/common/model/Spring.ts
+++ js/common/model/Spring.ts
@@ -38,6 +38,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import Disposable from '../../../../axon/js/Disposable.js';
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
@@ -283,7 +284,7 @@ export default class Spring extends PhetioObject {
   }
 
   public override dispose(): void {
-    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
+    Disposable.assertNotDisposable();
     super.dispose();
   }
 
