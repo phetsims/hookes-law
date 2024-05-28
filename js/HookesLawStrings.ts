50f2d04ae7442e49b34119cc27828934c348f452
pixelzoom <cmalley@pixelzoom.com>
2024-05-28 11:39:22 -0600

    supportsDynamicLocale: true, grunt modulify, https://github.com/phetsims/hookes-law/issues/81

--- js/HookesLawStrings.ts
+++ js/HookesLawStrings.ts
@@ -9,69 +9,40 @@ import type LocalizedStringProperty from '../../chipper/js/LocalizedStringProper
 import hookesLaw from './hookesLaw.js';
 
 type StringsType = {
-  'appliedForce': string;
   'appliedForceStringProperty': LocalizedStringProperty;
-  'appliedForceColon': string;
   'appliedForceColonStringProperty': LocalizedStringProperty;
-  'appliedForceNumber': string;
   'appliedForceNumberStringProperty': LocalizedStringProperty;
-  'barGraph': string;
   'barGraphStringProperty': LocalizedStringProperty;
-  'bottomSpring': string;
   'bottomSpringStringProperty': LocalizedStringProperty;
-  'components': string;
   'componentsStringProperty': LocalizedStringProperty;
-  'displacement': string;
   'displacementStringProperty': LocalizedStringProperty;
-  'displacementColon': string;
   'displacementColonStringProperty': LocalizedStringProperty;
-  'energy': string;
   'energyStringProperty': LocalizedStringProperty;
-  'energyPlot': string;
   'energyPlotStringProperty': LocalizedStringProperty;
-  'equilibriumPosition': string;
   'equilibriumPositionStringProperty': LocalizedStringProperty;
-  'forcePlot': string;
   'forcePlotStringProperty': LocalizedStringProperty;
   'hookes-law': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
-  'intro': string;
   'introStringProperty': LocalizedStringProperty;
-  'joules': string;
   'joulesStringProperty': LocalizedStringProperty;
-  'leftSpring': string;
   'leftSpringStringProperty': LocalizedStringProperty;
-  'meters': string;
   'metersStringProperty': LocalizedStringProperty;
-  'newtons': string;
   'newtonsStringProperty': LocalizedStringProperty;
-  'newtonsPerMeter': string;
   'newtonsPerMeterStringProperty': LocalizedStringProperty;
   'pattern': {
     '0value': {
-      '1units': string;
       '1unitsStringProperty': LocalizedStringProperty;
     }
   };
-  'potentialEnergy': string;
   'potentialEnergyStringProperty': LocalizedStringProperty;
-  'rightSpring': string;
   'rightSpringStringProperty': LocalizedStringProperty;
-  'springConstant': string;
   'springConstantStringProperty': LocalizedStringProperty;
-  'springConstantNumber': string;
   'springConstantNumberStringProperty': LocalizedStringProperty;
-  'springForce': string;
   'springForceStringProperty': LocalizedStringProperty;
-  'systems': string;
   'systemsStringProperty': LocalizedStringProperty;
-  'topSpring': string;
   'topSpringStringProperty': LocalizedStringProperty;
-  'total': string;
   'totalStringProperty': LocalizedStringProperty;
-  'values': string;
   'valuesStringProperty': LocalizedStringProperty;
 };
 
