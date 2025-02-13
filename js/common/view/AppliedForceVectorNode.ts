23f77730c4d695ffeb84f1cfe7cfc9d892841fd1
phet-dev <phetadmin@gmail.com>
2025-02-13 03:25:22 -0700

    update copyright dates from daily grunt work

--- js/common/view/AppliedForceVectorNode.ts
+++ js/common/view/AppliedForceVectorNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2023, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * AppliedForceVectorNode is the vector representation of applied force (F).
--- js/common/view/HookesLawSpringNode.ts
+++ js/common/view/HookesLawSpringNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * HookesLawSpringNode is a specialization of ParametricSpringNode that adapts it to the Hooke's Law spring model.
